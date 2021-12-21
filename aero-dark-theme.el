e0e2ed42cbc041a51a24a341e0c304a6fb4c513f
Jade Michael Thornton <jademichael@jmthornton.net>
2021-12-21 13:12:25 -0600

    add styling for mode-line-active

--- aero-dark-theme.el
+++ aero-dark-theme.el
@@ -126,6 +126,7 @@
    `(fringe ((t (:background ,(if-solaire aero-bg-alt aero-bg)))))
    `(cursor ((t (:background ,aero-fg))))
    `(mode-line ((t (:background ,aero-bg-alt :foreground ,aero-space-base :box (:line-width 4 :color ,aero-bg-alt)))))
+   `(mode-line-active ((t (:inherit mode-line))))
    `(mode-line-inactive ((t (:inherit mode-line :foreground ,aero-space-base-dim))))
    `(header-line ((t (:inherit mode-line :overline nil :underline nil
                       :box (:line-width 2 :color ,aero-bg-magenta) :height ,aero-theme-font-height))))
--- aero-theme.el
+++ aero-theme.el
@@ -178,6 +178,7 @@
      ((t (:background ,aero-modeline-bg
           :foreground ,aero-space-base :height ,aero-theme-font-height
           :underline nil :overline ,aero-fg :box nil))))
+   `(mode-line-active ((t (:inherit mode-line))))
    `(mode-line-inactive ((t (:inherit mode-line :foreground ,aero-space-base-dim))))
    `(header-line
      ((t (:inherit mode-line :height ,aero-theme-font-height
